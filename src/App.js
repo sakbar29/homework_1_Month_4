cc9a4d1240f15115d1f0a284dd0238d387625e42
akbar sydykov <sydykovakbar966@gmail.com>
2023-04-18 22:51:08 +0600

    homework_1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Remarkable } from "remarkable";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Plice extends React.Component{
+  constructor(props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {value : "hello world"};
+  
+  }
+  handleChange(e) {
+    this.setState({value: e.target.value})
+  }
+  getRawMarkup () {
+    const md = new Remarkable();
+    return { __html: md.render(this.state.value)};
+  }
+
+  render () {
+    return (
+      <div className="Plice"> 
+      <h3>enter data</h3>
+      <label htmlFor="markdown-content">enter form</label>
+      <textarea 
+        id="markdown-content" 
+        onChange={this.handleChange}
+        defaultValue={this.state.value}
+      />
+      <h3>conclusion</h3>
+      <div 
+        className="content"
+        dangerouslySetInnerHTML={this.getRawMarkup()}
+      />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Plice;
